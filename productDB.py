6d942aafec507175931bc68eb3df3edbac05c0eb
irfan8571 <irfannisho8571@gmail.com>
2020-08-07 22:05:53 +0600

    UserCart and UserOrder class added

--- productDB.py
+++ productDB.py
@@ -1,10 +1,23 @@
 from mongoengine import *
 
 
-class ProductGeneral(Document):
+class Product(Document):
     product_name = StringField(max_length=100)
     available_unit = FloatField()
     per_unit_charge = FloatField()
     product_image = StringField(max_length=1000)
     product_description = StringField(max_length=1000)
+
+class UserCart(Documet): 
+	user_email = EmailField(required=True)
+	product_Id = StringField(max_length=1000)
+	product_amount = FloatField()
+	total_price = FloatField()
+
+class UserOder(Document):
+	user_email = EmailField(required=True) 
+	product_Id = StringField(max_length=1000)
+	product_amount = FloatField()
+	total_price = FloatField()
+	date = DateField()    
 
