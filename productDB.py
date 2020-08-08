d5aedcaa1baabccf0977ea37c807833ba847133d
irfan8571 <irfannisho8571@gmail.com>
2020-08-08 19:24:40 +0600

    add_product method added in ProductDb class

--- productDB.py
+++ productDB.py
@@ -1,23 +1,48 @@
 from mongoengine import *
 
-
 class Product(Document):
     product_name = StringField(max_length=100)
-    available_unit = FloatField()
+    available_unit = IntField()
     per_unit_charge = FloatField()
     product_image = StringField(max_length=1000)
     product_description = StringField(max_length=1000)
 
-class UserCart(Documet): 
+
+class UserCart(Document): 
 	user_email = EmailField(required=True)
 	product_Id = StringField(max_length=1000)
-	product_amount = FloatField()
+	product_amount = IntField()
 	total_price = FloatField()
+
 
 class UserOder(Document):
 	user_email = EmailField(required=True) 
 	product_Id = StringField(max_length=1000)
-	product_amount = FloatField()
+	product_amount = IntField()
 	total_price = FloatField()
 	date = DateField()    
 
+
+class ProductDb():
+
+    def __init__(self):
+        pass	
+
+    def add_product(self, product_name, available_unit, per_unit_charge, product_image, product_description):
+	    product = Product(product_name=product_name, available_unit=available_unit, per_unit_charge=per_unit_charge, product_image=product_image, product_description=product_description)
+	    product.save()
+	    return {"Success" : True}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	                  
