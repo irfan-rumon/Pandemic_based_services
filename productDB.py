035220b51dcef6ec9b88b7ec0638b8154f2e68ee
irfan8571 <irfannisho8571@gmail.com>
2020-08-12 21:32:24 +0600

    increase_product method added to productDb

--- productDB.py
+++ productDB.py
@@ -1,11 +1,11 @@
 from mongoengine import *
 
 class Product(Document):
-    product_name = StringField(max_length=100)
-    available_unit = IntField()
-    per_unit_charge = FloatField()
-    product_image = StringField(max_length=1000)
-    product_description = StringField(max_length=1000)
+	product_name = StringField(max_length=100)
+	available_unit = IntField()
+	per_unit_charge = FloatField()
+	product_image = StringField(max_length=1000)
+	product_description = StringField(max_length=1000)
 
 
 class UserCart(Document): 
@@ -25,13 +25,25 @@ class UserOder(Document):
 
 class ProductDb():
 
-    def __init__(self):
-        pass	
+	def __init__(self):
+		pass	
 
-    def add_product(self, product_name, available_unit, per_unit_charge, product_image, product_description):
-	    product = Product(product_name=product_name, available_unit=available_unit, per_unit_charge=per_unit_charge, product_image=product_image, product_description=product_description)
-	    product.save()
-	    return {"Success" : True}
+	def add_product(self, product_name, available_unit, per_unit_charge, product_image, product_description):
+		#add produts  to product tabel
+		product = Product(product_name=product_name, available_unit=available_unit, per_unit_charge=per_unit_charge, product_image=product_image, product_description=product_description)
+		product.save()
+		return {"Success" : True}     #successfully added the product
+
+	def increase_product(self, productId, amount): 
+		#add  amount to availabe_unit field for given productId to product table
+		product = Product.objects(pk = productId )
+		if product:                                     #product found with that productId 
+			product = product.get(pk = productId )
+			newAmount = product.available_unit + amount
+			product.update(available_unit = newAmount)
+			return {"Success" : True}                   #successfully incresed amount of that product
+		else:                                           #No product found with that productId
+			return {"Success" : False}
 
 
 
@@ -45,4 +57,8 @@ class ProductDb():
 
 
 
-	                  
+
+
+
+
+					  
