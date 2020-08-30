eb836905333b3011f33fd33741a0e34c4d0e7c28
Minhaj <minhaj0231@gmail.com>
2020-08-30 13:52:50 +0600

    updated shop.py and productDb

--- shop.py
+++ shop.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from productDB import ProductDb
+from middleware import is_loggedIn, has_permission 
 shop = Blueprint("shop",__name__, static_folder="static", template_folder="templates")
+
 
 productDb = ProductDb()
 
@@ -8,14 +10,14 @@ productDb = ProductDb()
 def getAllProducts():
 
     productList = productDb.get_all_products()
-
-    return render_template('allProducts.html',products = productList['products'])
+    
+    return render_template('allProducts.html',products = productList)
 
 
 @shop.route("/getProductDetails/<product_Id>", methods =["GET"] )
 def getProductDetails(product_Id):
 
-    productDetails = productDb.get_singelProduct(product_Id)
+    productDetails = productDb.get_singleProduct(product_Id)
     print(productDetails)
 
     return render_template('productDetail.html',product = productDetails['product'])
@@ -37,3 +39,45 @@ def postIncreaseProduct():
     
 
     return redirect(url_for('shop.getProductDetails', product_Id=product_Id ))
+
+
+@shop.route("/getAddToCart/<product_Id>/<product_amount>", methods =["GET"])
+@is_loggedIn    
+def getAddToCart(product_Id,product_amount):
+
+    productDetails = productDb.get_singleProduct(product_Id)
+
+    product_name = productDetails['product_name']
+    total_price = productDetails['per_unit_charge'] * product_amount
+
+    try:
+
+        result = productDb.add_to_cart(
+            user_email= session['UserEmail'],
+            product_Id= product_Id,
+            product_name=product_name,
+            product_amount=product_amount,
+            total_price=total_price)
+        if result["Success"]:
+            flash("Added to Cart") 
+    except Exception as e:
+        flash("Error Try again") 
+
+    return redirect(url_for('shop.getAllProducts'))
+
+
+
+@shop.route("/getRemoveFromCart/<product_Id>", methods =["GET"])
+@is_loggedIn    
+def getRemoveFromCart(product_Id):
+
+    try:
+        result = productDb.remove_from_cart(user_email= session['UserEmail'], product_Id=product_Id)
+
+        if result["Success"] :
+                flash("Added to Cart") 
+    except Exception as e:
+        flash("Error Try again") 
+
+    return redirect(url_for('shop.getAllProducts'))
+
